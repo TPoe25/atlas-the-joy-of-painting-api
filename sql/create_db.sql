7771d08432797564451fe2adf29bc3e8a75c35c8
Taylor Poe <t_poe25@yahoo.com>
2025-12-02 14:49:35 -0600

    created locally, working on scripts

---
+++ sql/create_db.sql
@@ -0,0 +1,39 @@
+DROP TABLE IF EXISTS episode_subjects;
+DROP TABLE IF EXISTS episode_colors;
+DROP TABLE IF EXISTS episodes;
+DROP TABLE IF EXISTS colors;
+DROP TABLE IF EXISTS subjects;
+
+CREATE TABLE episodes (
+    id SERIAL PRIMARY KEY,
+    title TEXT UNIQUE,
+    season INT,
+    episode INT,
+    air_date DATE,
+    month_of_air INT,
+    img_url TEXT,
+    youtube_url TEXT
+);
+
+CREATE TABLE colors (
+    id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE,
+    hex TEXT
+);
+
+CREATE TABLE subjects (
+    id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE
+);
+
+CREATE TABLE episode_colors (
+    episode_id INT REFERENCES episodes(id) ON DELETE CASCADE,
+    color_id INT REFERENCES colors(id) ON DELETE CASCADE,
+    PRIMARY KEY (episode_id, color_id)
+);
+
+CREATE TABLE episode_subjects (
+    episode_id INT REFERENCES episodes(id) ON DELETE CASCADE,
+    subject_id INT REFERENCES subjects(id) ON DELETE CASCADE,
+    PRIMARY KEY (episode_id, subject_id)
+);
